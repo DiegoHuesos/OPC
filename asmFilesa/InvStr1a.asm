05acc96f60453f85b65ae60358a3088fa33c4d90
DiegoHuesos <diegohd_98@hotmail.com>
2022-11-08 12:48:15 -0600

    Invertir String con Stack Added

--- asmFilesa/InvStr1a.asm
+++ asmFilesa/InvStr1a.asm
@@ -1,24 +1,55 @@
-TITLE Program Template          (InvStr.asm)
+TITLE Invierte String     (InvStr.asm)
 
 ; Este programa invierte un String.
+; Diego Hernández Delgado
+; 176262
 
 INCLUDE Irvine32.inc
 
 .data
 unaCade BYTE "Gabriel Garcia Marquez",0
 tamCade DWORD LENGTHOF unaCade - 1
+endCade DWORD ?
+
 
 .code
 main PROC
 
+	; Define la dirección donde termina el arreglo en la variable endCade = offset unaCade + tamCade
+	mov eax, offset unaCade
+	add endCade, eax
+	mov eax, tamCade
+	add endCade, eax
 
-    ; Despliega el string invertido.
+	;Imprime la cadena original
+	call Crlf
 	call Crlf
 	mov edx, OFFSET unaCade
-	movzx eax, edx 
-	.WHILE( )
-	.ENDW
 	call Writestring
+	call Crlf
+
+	;Push the elements of the array into the stack
+	mov esi, OFFSET unaCade		;Recordar que ESI y EDI se usan para guardar direcciones de memoria
+	.WHILE( esi < endCade )
+		mov ax, [esi]	
+		push ax					;La unidad mínima que maneja el stack son 2 bytes (word)--> ax
+		inc esi
+	.ENDW
+
+	;Pop and move the elements of the stack into the array in the new order
+	mov esi, OFFSET unaCade
+	.WHILE( esi < endCade )
+		pop ax	
+		mov [esi], al		;Aunque popeamos ax (2 bytes), el valor que nos interesa (1 byte) está en al
+		inc esi 
+	.ENDW
+
+
+	;Imprime la cadena ya invertida
+	call Crlf
+	mov edx, OFFSET unaCade
+	call Writestring
+	call Crlf
 	call Crlf
 
 	EXIT
